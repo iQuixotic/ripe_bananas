5f5cb46c4c12149d0e14065ce26d054ef3eb6034
Trey <treyw1850@gmail.com>
2019-08-15 06:44:14 -0500

    accordian

--- client/src/api/reviews.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default {
-    getSingleMovie: (title: string, year: string) => {
-        const url = ` http://www.omdbapi.com/?apikey=${SECRET.MOVIE_API_KEY}&t=${title}&y=${year}`;
-        return axios.get(url);
-    }
-}
