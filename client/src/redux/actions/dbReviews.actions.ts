4f83d71663c144c1e930ffaa5abdb256d18f15c4
Trey <treyw1850@gmail.com>
2019-08-14 19:32:14 -0500

    set up reducers and actions for reviews

---
+++ client/src/redux/actions/dbReviews.actions.ts
@@ -0,0 +1,75 @@
+export const reviewTypes = {
+    REVIEW_RATING_UPDATE: 'REVIEW_RATING_UPDATE',
+    REVIEW_TITLE_UPDATE: 'REVIEW_TITLE_UPDATE',
+    REVIEW_BODY_UPDATE: 'REVIEW_BODY_UPDATE',
+    REVIEW_FIRSTNAME_UPDATE:'REVIEW_FIRSTNAME_UPDATE',
+    REVIEW_LASTNAME_UPDATE:'REVIEW_LASTNAME_UPDATE',
+    SINGLE_REVIEW_REQ:'SINGLE_REVIEW_REQ'
+
+};
+
+
+
+export const reviewRatingUpdate = 
+    (reviewRating: string) => (dispatch: any) => {
+        dispatch({
+            payload: {
+                reviewRating
+            },
+            type: reviewTypes.REVIEW_RATING_UPDATE
+        });
+    }
+
+export const reviewTitleUpdate = 
+    (reviewTitle: string) => (dispatch: any) => {
+        dispatch({
+            payload: {
+                reviewTitle
+            },
+            type: reviewTypes.REVIEW_TITLE_UPDATE
+        });
+    }
+
+export const reviewBodyUpdate = 
+    (reviewBody: string) => (dispatch: any) => {
+        dispatch({
+            payload: {
+                reviewBody
+            },
+            type: reviewTypes.REVIEW_BODY_UPDATE
+        });
+    }
+
+export const reviewFirstNameUpdate = 
+    (reviewFirstName: string) => (dispatch: any) => {
+        dispatch({
+            payload: {
+                reviewFirstName
+            },
+            type: reviewTypes.REVIEW_FIRSTNAME_UPDATE
+        });
+    }
+
+export const reviewLastNameUpdate = 
+    (reviewLastName: string) => (dispatch: any) => {
+        dispatch({
+            payload: {
+                reviewLastName
+            },
+            type: reviewTypes.REVIEW_LASTNAME_UPDATE
+        });
+    }
+
+    export const singleReviewMessageReq = () => (dispatch: any) => {
+        dispatch({
+                payload: {},
+            type: reviewTypes.SINGLE_REVIEW_REQ
+        });
+    }
+
+
+
+
+
+
+
